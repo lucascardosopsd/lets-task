1984b02c22a5cb022aef95fc75e45377b95a1171
lucas cardoso <lucascardosopsd@gmail.com>
2023-12-25 19:17:41 -0300

    fix: sidebar layout

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -17,9 +17,9 @@ const Sidebar = () => {
   };
 
   return (
-    <div className="h-full p-4 w-full">
+    <>
       <div
-        className={`absolute top-4 left-4 bg-zinc-800 flex items-center text-zinc-600 p-2 rounded-md tablet:hidden transition ${
+        className={`absolute top-2 left-2 bg-zinc-800 flex items-center text-zinc-600 p-2 rounded-md tablet:hidden transition ${
           isOpen ? "hidden tablet:visible" : "visible"
         } `}
         onClick={toggle}
@@ -27,28 +27,30 @@ const Sidebar = () => {
         <MdChevronRight size="20" />
       </div>
       <div
-        className={`flex flex-col justify-between items-center box h-[95%] my-auto top-0 bottom-0 left-5 tablet:h-full w-1/2 tablet:w-1/6 text-zinc-400 absolute tablet:static transition ${
-          isOpen ? "translate-x-0" : "-translate-x-96 tablet:translate-x-0"
+        className={`flex flex-col justify-between items-center box !rounded-none tablet:rounded h-full my-auto w-full tablet:w-full text-zinc-400 absolute tablet:static transition ${
+          isOpen ? "translate-x-0" : "-translate-x-full tablet:translate-x-0"
         }`}
       >
-        <div
-          className="flex flex-col gap-2 items-center relative"
-          onClick={toggle}
-        >
+        <span className="flex items-center justify-center w-full">
           <MdChevronLeft
             size="25"
-            className="absolute -right-8 -top-2 text-zinc-700 tablet:hidden"
+            className="text-zinc-700 tablet:hidden absolute right-2 top-2"
           />
-          <div className="relative h-16 w-16">
-            <Image
-              src={session?.user?.image ?? "/images/fake-user"}
-              className="rounded-full object-cover h-full w-full border border-green-500"
-              fill
-              alt="Foto usuário"
-            />
+          <div
+            className="flex flex-col gap-2 items-center relative"
+            onClick={toggle}
+          >
+            <div className="relative h-16 w-16">
+              <Image
+                src={session?.user?.image ?? "/images/fake-user"}
+                className="rounded-full object-cover h-full w-full border border-green-500"
+                fill
+                alt="Foto usuário"
+              />
+            </div>
+            <p className="font-medium break-words">{session?.user?.name}</p>
           </div>
-          <p className="font-medium break-words">{session?.user?.name}</p>
-        </div>
+        </span>
 
         <ul className="gap-8 w-full">
           {sidebarLinks.map(({ icon, label, title }, index) => (
@@ -64,7 +66,7 @@ const Sidebar = () => {
           <p>Sair</p>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
