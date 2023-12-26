8b9016626f1c2c52287e5575141c17979f6b2b34
lucas cardoso <lucascardosopsd@gmail.com>
2023-12-25 22:10:56 -0300

    fix: mongodb connection

--- src/config/mongoDb.ts
+++ src/config/mongoDb.ts
@@ -1,6 +1,12 @@
 import mongoose from "mongoose";
 
-const connectDb = async () =>
-  mongoose.connect(process.env.NEXT_PUBLIC_MONGO_URI as string);
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI as string);
+    console.log("Mongo Connected");
+  } catch (error) {
+    throw new Error("Error when connect mongodb");
+  }
+};
 
 export default connectDb;
