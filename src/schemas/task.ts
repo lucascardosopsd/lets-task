2c8dab5f57dd8a3c4c5c468134e41aa5c8cee2c3
lucas cardoso <lucascardosopsd@gmail.com>
2023-12-26 12:29:40 -0300

    update: add timestamps to task schema

--- src/schemas/task.ts
+++ src/schemas/task.ts
@@ -1,25 +1,30 @@
 import { Schema, model, models } from "mongoose";
 
-const taskSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
+const taskSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    complete: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    important: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
   },
-  description: {
-    type: String,
-    required: true,
-  },
-  complete: {
-    type: Boolean,
-    required: true,
-    default: false,
-  },
-  important: {
-    type: Boolean,
-    required: true,
-    default: false,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Task = models?.Task || model("Task", taskSchema);
 
