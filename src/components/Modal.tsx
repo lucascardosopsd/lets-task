a4cdd0b6053a418a3b474a7b0fc787062b1439e4
lucas cardoso <lucascardosopsd@gmail.com>
2023-12-26 20:25:15 -0300

    feat: close modal when click background

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -22,37 +22,45 @@ const Modal = ({
   loading,
 }: ModalProps) => {
   return (
-    <form
-      className={`flex transition ${
-        isOpen ? "translate-y-0" : "-translate-y-[1000px]"
-      } absolute h-[600px] w-[600px] top-0 bottom-0 right-0 left-0 mx-auto my-auto flex-col gap-2 bg-zinc-900 rounded box !pb-0 z-10`}
-      onSubmit={onSubmit}
-    >
-      <div className="relative flex justify-center border-b border-zinc-800 text-zinc-200 py-4">
-        <MdClose
-          className="absolute top-0 right-3 text-zinc-600 hover:text-green-500 cursor-pointer transition"
-          size="20"
-          onClick={onClose}
-        >
-          X
-        </MdClose>
-        {header}
-      </div>
-      <div className="h-[500px] overflow-y-auto p-4">{body}</div>
-      <footer className="flex w-full justify-stretch">
-        <button className="btn btn-secondary w-full" onClick={onClose}>
-          Cancelar
-        </button>
-        <button
-          className={`btn btn-primary w-full ${
-            loading && "pointer-events-none cursor-not-allowed"
-          }`}
-          type="submit"
-        >
-          Enviar
-        </button>
-      </footer>
-    </form>
+    <>
+      <form
+        className={`flex transition ${
+          isOpen ? "translate-y-0" : "-translate-y-[1000px]"
+        } absolute h-[600px] w-[600px] top-0 bottom-0 right-0 left-0 mx-auto my-auto flex-col gap-2 bg-zinc-900 rounded box !pb-0 z-10`}
+        onSubmit={onSubmit}
+      >
+        <div className="relative flex justify-center border-b border-zinc-800 text-zinc-200 py-4">
+          <MdClose
+            className="absolute top-0 right-3 text-zinc-600 hover:text-green-500 cursor-pointer transition"
+            size="20"
+            onClick={onClose}
+          >
+            X
+          </MdClose>
+          {header}
+        </div>
+        <div className="h-[500px] overflow-y-auto p-4">{body}</div>
+        <footer className="flex w-full justify-stretch">
+          <button className="btn btn-secondary w-full" onClick={onClose}>
+            Cancelar
+          </button>
+          <button
+            className={`btn btn-primary w-full ${
+              loading && "pointer-events-none cursor-not-allowed"
+            }`}
+            type="submit"
+          >
+            Enviar
+          </button>
+        </footer>
+      </form>
+      <div
+        className={`h-full w-full bg-black bg-opacity-40 top-0 left-0 absolute ${
+          !isOpen ? "hidden" : "block"
+        }`}
+        onClick={onClose}
+      />
+    </>
   );
 };
 
