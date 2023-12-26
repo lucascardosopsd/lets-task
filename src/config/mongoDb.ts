956abb3a21a568e09602dc701832e99b12898db5
lucas cardoso <lucascardosopsd@gmail.com>
2023-12-26 00:04:32 -0300

    check: aready connected to mongo

--- src/config/mongoDb.ts
+++ src/config/mongoDb.ts
@@ -3,6 +3,8 @@
 import mongoose from "mongoose";
 
 const connectDb = async () => {
+  if (mongoose?.connections[0]?.readyState) return;
+
   try {
     await mongoose.connect(process.env.MONGO_URI as string);
     console.log("Mongo Connected");
