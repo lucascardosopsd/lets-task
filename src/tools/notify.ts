0c5eea7781eb63e5fb05e945559cb6f7a0f32d9f
lucas cardoso <lucascardosopsd@gmail.com>
2023-12-26 20:02:12 -0300

    update: notify position

--- src/tools/notify.ts
+++ src/tools/notify.ts
@@ -1,4 +1,4 @@
 import { TypeOptions, toast } from "react-toastify";
 
 export const notify = (type: TypeOptions, message: string) =>
-  toast(message, { type, theme: "dark" });
+  toast(message, { type, theme: "dark", position: "bottom-right" });
