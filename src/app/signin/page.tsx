99f8a2a5d18b26dca966f4ca10cd14e59fbd3b69
lucas cardoso <lucascardosopsd@gmail.com>
2023-12-26 22:32:59 -0300

    layout: responsivity in login page

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -5,11 +5,17 @@ import Image from "next/image";
 export default function Login() {
   return (
     <main className="flex items-center justify-center h-screen">
-      <div className="flex h-[400px] w-[600px] rounded">
+      <div className="flex flex-col mobile:flex-row w-full h-full mobile:h-[400px] mobile:w-[600px] rounded">
         <div className="flex w-full flex-1 bg-green-500 items-center justify-center rounded-l">
-          <Image src="/images/key.svg" alt="key" height={200} width={200} />
+          <Image
+            src="/images/key.svg"
+            alt="key"
+            height={200}
+            width={200}
+            className="h-28 w-28 mobile:h-52 mobile:w-52"
+          />
         </div>
-        <div className="flex flex-col w-full flex-1 items-center justify-center gap-4 bg-zinc-900 rounded-r">
+        <div className="flex flex-col w-full flex-[3] mobile:flex-1 items-center justify-center gap-4 bg-zinc-900 rounded-r">
           <Logo />
           <SignInGoogle />
         </div>
