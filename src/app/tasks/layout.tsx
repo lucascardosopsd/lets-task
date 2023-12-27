28655c810898569f0c5cc9b9cce8cf7d211372e5
lucas cardoso <lucascardosopsd@gmail.com>
2023-12-27 01:04:12 -0300

    fix: enable route protection to tasks route

--- src/app/tasks/layout.tsx
+++ src/app/tasks/layout.tsx
@@ -1,15 +1,16 @@
 "use client";
+import ProtectRoute from "@/components/ProtectRoute";
 import Sidebar from "@/components/Sidebar";
 import { ReactNode } from "react";
 
 const layout = ({ children }: { children: ReactNode }) => {
   return (
-    // <ProtectRoute>
-    <div className="mobile:flex h-screen">
-      <Sidebar />
-      {children}
-    </div>
-    // </ProtectRoute>
+    <ProtectRoute>
+      <div className="mobile:flex h-screen">
+        <Sidebar />
+        {children}
+      </div>
+    </ProtectRoute>
   );
 };
 
