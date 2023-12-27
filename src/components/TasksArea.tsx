ccd4e58730ee267204992f578430bcbef308bd85
lucas cardoso <lucascardosopsd@gmail.com>
2023-12-27 02:09:30 -0300

    fix: complete tasks filter

--- src/components/TasksArea.tsx
+++ src/components/TasksArea.tsx
@@ -65,7 +65,7 @@ const TasksArea = ({ data }: TasksAreaProps) => {
       >
         +
       </button>
-      {!filtered.length
+      {!filtered.length && currentLabel == "all"
         ? data.map((task, index) => <TaskCard task={task} key={index} />)
         : filtered.map((task, index) => <TaskCard task={task} key={index} />)}
       <Modal
