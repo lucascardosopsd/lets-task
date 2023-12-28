ab7a6a47a879246886470a69a2886e1da744ae3c
lucas cardoso <lucascardosopsd@gmail.com>
2023-12-28 14:16:11 -0300

    fix: dele many revalidate path

--- src/services/tasks/deleteManyTasks.ts
+++ src/services/tasks/deleteManyTasks.ts
@@ -1,6 +1,7 @@
 "use server";
 import { connectDb } from "@/lib/mongoDb";
 import Task from "@/schemas/task";
+import { revalidatePath } from "next/cache";
 
 export default async function deleteManyTasks(ids: string[]) {
   await connectDb();
@@ -9,6 +10,7 @@ export default async function deleteManyTasks(ids: string[]) {
     await Task.deleteMany({
       _id: { $in: ids },
     });
+    revalidatePath("/tasks");
   } catch (error) {
     console.log(error);
     throw new Error("Erro when delete task.");
