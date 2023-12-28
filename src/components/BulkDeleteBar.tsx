82982816494b137a40c7f7129d07832b4f31dc82
lucas cardoso <lucascardosopsd@gmail.com>
2023-12-28 12:14:58 -0300

    refact: BulkDeleteBar to component

---
+++ src/components/BulkDeleteBar.tsx
@@ -0,0 +1,48 @@
+import { TaskProps } from "@/types/task";
+import { IoClose } from "react-icons/io5";
+
+interface BulkDeleteBarProps {
+  arrayDelete: TaskProps[];
+  handleRemoveFromDelete: (task: TaskProps) => void;
+  handleConfirmDeleteTasks: () => void;
+}
+
+const BulkDeleteBar = ({
+  arrayDelete,
+  handleConfirmDeleteTasks,
+  handleRemoveFromDelete,
+}: BulkDeleteBarProps) => {
+  return (
+    <div
+      className={`absolute flex translate-y-0 transition items-stretch gap-2 left-0 bottom-0 w-full  bg-zinc-900 border-t border-zinc-800 z-40 ${
+        !arrayDelete.length && "translate-y-full"
+      } `}
+    >
+      <div className="flex items-center gap-2 my-2 mx-2 overflow-x-auto w-full">
+        {arrayDelete.map((task, index) => (
+          <div className="flex h-10" key={index}>
+            <div className="rounded-l flex items-center text-start p-2 h-full w-40 line-clamp-1 truncate text-yellow-900 bg-yellow-600 relative">
+              <p>{task.title}</p>
+            </div>
+            <span
+              className="flex rounded-r items-center justify-center p-1 bg-yellow-500 hover:bg-yellow-600 transition h-full cursor-pointer"
+              onClick={() => handleRemoveFromDelete(task)}
+            >
+              <IoClose />
+            </span>
+          </div>
+        ))}
+      </div>
+      <div className="flex w-full justify-end">
+        <button
+          onClick={() => handleConfirmDeleteTasks()}
+          className="flex items-center justify-center px-2 rounded-none bg-red-600 text-red-950 transition hover:bg-red-800"
+        >
+          confirmar
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default BulkDeleteBar;
--- src/components/TasksArea.tsx
+++ src/components/TasksArea.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { TaskProps } from "@/types/task";
 import { notify } from "@/tools/notify";
 import createTask from "@/services/tasks/create";
@@ -10,7 +9,7 @@ import deleteManyTasks from "@/services/tasks/deleteManyTasks";
 import TaskCard from "./TaskCard";
 import Modal from "./Modal";
 import UpdateTaskBody from "./formBodies/UpdateTask";
-import { IoClose } from "react-icons/io5";
+import BulkDeleteBar from "./BulkDeleteBar";
 
 interface TasksAreaProps {
   data: TaskProps[];
@@ -136,35 +135,11 @@ const TasksArea = ({ data }: TasksAreaProps) => {
         <p className="group-hover:text-green-500">+</p>
       </button>
 
-      <div
-        className={`absolute flex translate-y-0 transition items-stretch gap-2 left-0 bottom-0 w-full  bg-zinc-900 border-t border-zinc-800 z-40 ${
-          !arrayDelete.length && "translate-y-full"
-        } `}
-      >
-        <div className="flex items-center gap-2 my-2 mx-2 overflow-x-auto w-full">
-          {arrayDelete.map((task, index) => (
-            <div className="flex h-10" key={index}>
-              <div className="rounded-l flex items-center text-start p-2 h-full w-40 line-clamp-1 truncate text-yellow-900 bg-yellow-600 relative">
-                <p>{task.title}</p>
-              </div>
-              <span
-                className="flex rounded-r items-center justify-center p-1 bg-yellow-500 hover:bg-yellow-600 transition h-full cursor-pointer"
-                onClick={() => handleRemoveFromDelete(task)}
-              >
-                <IoClose />
-              </span>
-            </div>
-          ))}
-        </div>
-        <div className="flex w-full justify-end">
-          <button
-            onClick={() => handleConfirmDeleteTasks()}
-            className="flex items-center justify-center px-2 rounded-none bg-red-600 text-red-950 transition hover:bg-red-800"
-          >
-            confirmar
-          </button>
-        </div>
-      </div>
+      <BulkDeleteBar
+        arrayDelete={arrayDelete}
+        handleConfirmDeleteTasks={handleConfirmDeleteTasks}
+        handleRemoveFromDelete={handleRemoveFromDelete}
+      />
 
       {!filtered.length &&
         !arrayDelete.length &&
